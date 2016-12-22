81b29e845350eb764a8f8c6afac57d05acb4bf16
anand.b <anand.b@whatarage.com>
2016-12-22 12:41:47 +0530

    port change

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ var server = http.createServer(app);
 /* Configuration */
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.set('port', 3000);
+app.set('port', 8080);
 
 if (process.env.NODE_ENV === 'development') {
-	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 }
 
 /* Socket.io Communication */
@@ -26,8 +26,8 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', socket);
 
 /* Start server */
-server.listen(app.get('port'), function (){
-  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+server.listen(app.get('port'), function() {
+    console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
 
 module.exports = app;
